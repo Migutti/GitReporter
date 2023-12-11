f4d871a59d56347be9d6c9360b4263f50bbc945a
Michael Guttmann <michael.guttmann@student.tugraz.at>
2023-12-11 10:15:20 +0100

    added basic test and requirements

---
+++ test-definitions/basic-test/init.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+testname="basic-test"
+
+rm -rf report
+mkdir report
+
+rm -rf tests/$testname
+mkdir tests/$testname
+cd tests/$testname
+
+git init --initial-branch=main
+git config user.name "Test1"
+git config user.email "testuser1@test.com"
+
+cp ../../test-definitions/$testname/version1.c main.c
+git add main.c
+git commit -m "Initial commit"
+
+git config user.name "Test2"
+git config user.email "testuser2@test.com"
+
+cp ../../test-definitions/$testname/version2.c main.c
+git add main.c
+git commit -m "Second commit"
+
+cd ../..
+python main.py -c test-definitions/$testname/config.json
---
+++ test-definitions/basic-test/version2.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+//------------------------------------------------------------------------------
+// Main Function
+//
+int main(void)
+{
+  int a = 1;
+  int b = 2;
+  printf("a + b = %d\n", a + b);
+
+  printf("Hello World!\n");
+  return 0;
+}
